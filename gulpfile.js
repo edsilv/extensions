e94f8d55b68306163f469ee04f31213355ea327e
edsilv <e.silverton@gmail.com>
2015-06-22 10:56:26 +0100

    moved bump to tasks/

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,14 @@
-var argv = require('yargs').argv,
-    bump = require('gulp-bump'),
-    Config = require('./gulpfile.config'),
+var Config = require('./gulpfile.config'),
+    config = new Config(),
     del = require('del'),
-    exec = require('child_process').exec,
     gulp = require('gulp'),
     merge = require('merge2'),
     mocha = require('gulp-mocha'),
     rename = require('gulp-rename'),
+    requireDir = require('require-dir'),
     runSequence = require('run-sequence'),
+    tasks = requireDir('./tasks'),
     ts = require('gulp-typescript');
-
-var config = new Config();
-
-gulp.task('bump', function(){
-    var bumpType = argv.type || 'patch'; // major.minor.patch
-
-    gulp.src(['./bower.json', './package.json'])
-        .pipe(bump({type: bumpType}))
-        .pipe(gulp.dest('./'));
-});
-
-// requires global gulp-cli
-gulp.task('bump:minor', function(cb){
-    exec('gulp bump --type minor', function (err, stdout, stderr) {
-        cb();
-    });
-});
-
-// requires global gulp-cli
-gulp.task('bump:major', function(cb){
-    exec('gulp bump --type major', function (err, stdout, stderr) {
-        cb();
-    });
-});
 
 gulp.task('test', function () {
     return gulp.src(config.test, {read: false})
